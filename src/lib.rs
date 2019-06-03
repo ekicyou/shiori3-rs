8437ab0a8489dbfbcf3fcb89c7837ae2ccd41e0a
ekicyou <dot.station@gmail.com>
2019-06-03 10:09:43 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod parsers;
 
 pub use crate::api::RawShiori3;
 pub use crate::api::Shiori3;
+pub use crate::error::MyError as ShioriError;
 pub use crate::error::MyErrorKind as ShioriErrorKind;
 pub use crate::error::MyResult as ShioriResult;
 pub use crate::parsers::req::ShioriRequest;
