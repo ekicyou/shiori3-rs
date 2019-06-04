2e80c016111a709fc316e7580c47025595c35ceb
ekicyou <dot.station@gmail.com>
2019-06-04 13:21:38 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,4 @@ pub use crate::error::MyError as ShioriError;
 pub use crate::error::MyErrorKind as ShioriErrorKind;
 pub use crate::error::MyResult as ShioriResult;
 pub use crate::parsers::req::ShioriRequest;
-pub use crate::parsers::req_parser::Rule;
+pub use crate::parsers::req_parser::Rule as ShioriParserRule;
