a9060306ea6858204d4739814cc76caa4e63a635
ekicyou <dot.station@gmail.com>
2019-06-13 11:34:31 +0900

    ShioriRequestParser

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,5 @@ pub use crate::error::MyError as ShioriError;
 pub use crate::error::MyErrorKind as ShioriErrorKind;
 pub use crate::error::MyResult as ShioriResult;
 pub use crate::parsers::req::ShioriRequest;
-pub use crate::parsers::req_parser::Rule as ShioriParserRule;
+pub use crate::parsers::req_parser::Rule as ShioriRequestParserRule;
+pub use crate::parsers::req_parser::ShioriRequestParser;
