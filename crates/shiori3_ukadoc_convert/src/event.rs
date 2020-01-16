065fd92ef2b554e31320b56335568b20db5bbc76
ekicyou <dot.station@gmail.com>
2020-01-16 17:12:07 +0900

    update

--- crates/shiori3_ukadoc_convert/src/event.rs
+++ crates/shiori3_ukadoc_convert/src/event.rs
@@ -2,18 +2,32 @@
 
 use html5ever::driver::ParseOpts;
 use html5ever::interface::tree_builder::TreeSink;
-use markup5ever_rcdom::RcDom;
+use markup5ever_rcdom::{Node, NodeData, RcDom};
+use std::cell::RefCell;
+use std::rc::Rc;
 use tendril::stream::TendrilSink;
+
+//fn iter_node(node:&Node)-> impl  std::iter::Map<std::slice::Iter<'_, std::rc::Rc<markup5ever_rcdom::Node>>, [closure@crates\shiori3_ukadoc_convert\src\event.rs:14:37: 14:63]>
+//&markup5ever_rcdom::NodeData
+//&std::cell::RefCell<std::vec::Vec<std::rc::Rc<markup5ever_rcdom::Node>>>
+
+struct RefNode<'a> {
+    data: &'a NodeData,
+    children: &'a RefCell<Vec<Rc<Node>>>,
+}
 
 pub fn get_events() {
     let html = crate::ukadoc::LIST_SHIORI_EVENT;
     let parser = html5ever::parse_document(RcDom::default(), ParseOpts::default());
-    let mut dom = parser.one(html);
-    let doc = dom.get_document();
+    let dom = parser.one(html);
+    let doc = &dom.document;
     let children = doc.children.borrow();
-    let items = children.iter().map(|n| (&n.data, &n.children)); //.take(2);
-    for (data, children) in items {
-        println!("{:?}", data);
+    let items = children.iter().map(|n| RefNode {
+        data: &n.data,
+        children: &n.children,
+    }); //.take(2);
+    for node in items {
+        println!("{:?}", node.data);
     }
 }
 
