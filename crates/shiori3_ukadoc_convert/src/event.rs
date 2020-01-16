752dd26c564d3b098896347d7601ecc663b8e115
ekicyou <dot.station@gmail.com>
2020-01-16 15:52:09 +0900

    build

--- crates/shiori3_ukadoc_convert/src/event.rs
+++ crates/shiori3_ukadoc_convert/src/event.rs
@@ -1,23 +1,17 @@
 // https://qiita.com/nacika_ins/items/c618c503cdc0080c7db8
 
-
-use markup5ever_rcdom as rcdom;
-
-use std::cell::RefCell;
-use std::rc::Rc;
-use html5ever::{Attribute, LocalName, QualName};
+use markup5ever_rcdom::{RcDom};
 use html5ever::driver::ParseOpts;
-use html5ever::{local_name, ns, namespace_url};
-use html5ever::{parse_document, parse_fragment};
-use html5ever::serialize;
-use html5ever::serialize::SerializeOpts;
-use html5ever::tendril::StrTendril;
-use html5ever::tendril::stream::TendrilSink;
-use rcdom::{Handle, Node, NodeData, RcDom};
+use tendril::stream::TendrilSink;
 
-pub fn get_events(){
+pub fn get_events() -> (){
     let html = crate::ukadoc::LIST_SHIORI_EVENT;
-    let parser = parse_document(RcDom::default(), ParseOpts::default());
-let dom = parser.one(html);
+    let parser = html5ever::parse_document(RcDom::default(), ParseOpts::default());
+    let dom = parser.one(html);
+    ()
+}
 
+#[test]
+fn read_event(){
+    get_events();
 }
