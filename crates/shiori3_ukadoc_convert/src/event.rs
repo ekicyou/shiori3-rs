3acec31eb49492e6cdfcb2e948fd339d58bb591c
ekicyou <dot.station@gmail.com>
2020-01-17 13:57:35 +0900

    entryの列挙

--- crates/shiori3_ukadoc_convert/src/event.rs
+++ crates/shiori3_ukadoc_convert/src/event.rs
@@ -2,47 +2,128 @@
 
 use html5ever::driver::ParseOpts;
 use html5ever::interface::tree_builder::TreeSink;
-use markup5ever_rcdom::{Node, NodeData, RcDom};
+use html5ever::{local_name, LocalName};
+use markup5ever_rcdom::{Handle, NodeData, RcDom};
 use std::cell::RefCell;
 use std::rc::Rc;
 use tendril::stream::TendrilSink;
+use tendril::StrTendril;
 
 //fn iter_node(node:&Node)-> impl  std::iter::Map<std::slice::Iter<'_, std::rc::Rc<markup5ever_rcdom::Node>>, [closure@crates\shiori3_ukadoc_convert\src\event.rs:14:37: 14:63]>
 //&markup5ever_rcdom::NodeData
 //&std::cell::RefCell<std::vec::Vec<std::rc::Rc<markup5ever_rcdom::Node>>>
 
-struct RefNode<'a> {
-    data: &'a NodeData,
-    children: &'a RefCell<Vec<Rc<Node>>>,
+struct FlatMapHandle {
+    owner: Handle,
+    index: usize,
+    child_iter: Option<Box<dyn Iterator<Item = Handle>>>,
 }
 
-struct Children {
-    children: RefCell<Vec<Rc<Node>>>,
-}
-/*
-impl Iterator<Item = RefNode> for Children {
-    fn next(&mut self) -> Option<Self::Item> {
-        None
+impl FlatMapHandle {
+    pub fn new(owner: &Handle) -> FlatMapHandle {
+        FlatMapHandle {
+            owner: owner.clone(),
+            index: 0,
+            child_iter: Some(Box::new(Some(owner.clone()).into_iter())),
+        }
     }
 }
-fn iter_ref_node(children: &RefCell<Vec<Rc<Node>>>) -> impl Iterator<Item = RefNode> {
-    let children = children.clone();
-    let items = children.iter().map(|n| RefNode {
-        data: &n.data,
-        children: &n.children,
-    });
-    items
+
+impl Iterator for FlatMapHandle {
+    type Item = Handle;
+    fn next(&mut self) -> Option<Handle> {
+        loop {
+            match &mut self.child_iter {
+                Some(iter) => {
+                    if let Some(item) = iter.next() {
+                        return Some(item);
+                    }
+                    let v = self.owner.children.borrow();
+                    if v.len() <= self.index {
+                        self.child_iter = None;
+                        return None;
+                    } else {
+                        self.child_iter = Some(Box::new(Self::new(&v[self.index])));
+                        self.index += 1;
+                        continue;
+                    }
+                }
+                _ => return None,
+            }
+        }
+    }
 }
-*/
+
+trait HandleExt {
+    fn iter_self_and_descendant(&self) -> FlatMapHandle;
+    fn is_element(&self) -> bool;
+    fn is_name(&self, local_name: LocalName) -> bool;
+    fn id(&self) -> Option<StrTendril>;
+    fn has_id(&self) -> bool {
+        self.id().is_some()
+    }
+}
+impl HandleExt for Handle {
+    fn iter_self_and_descendant(&self) -> FlatMapHandle {
+        FlatMapHandle::new(self)
+    }
+    fn id(&self) -> Option<StrTendril> {
+        match &self.data {
+            NodeData::Element { attrs, .. } => {
+                let id = attrs
+                    .borrow()
+                    .iter()
+                    .filter(|a| a.name.local == local_name!("id"))
+                    .next()
+                    .map(|a| a.value.clone());
+                id
+            }
+            _ => None,
+        }
+    }
+    #[inline]
+    fn is_element(&self) -> bool {
+        match &self.data {
+            NodeData::Element { .. } => true,
+            _ => false,
+        }
+    }
+    #[inline]
+    fn is_name(&self, local_name: LocalName) -> bool {
+        match &self.data {
+            NodeData::Element { name, .. } => name.local == local_name,
+            _ => false,
+        }
+    }
+}
 
 pub fn get_events() {
     let html_text = crate::ukadoc::LIST_SHIORI_EVENT;
     let parser = html5ever::parse_document(RcDom::default(), ParseOpts::default());
     let dom = parser.one(html_text);
-    let doc = &dom.document;
-    let children = doc.children.borrow();
-    for node in children.iter() {
-        println!("{:?}", node.data);
+    let dom_children = dom.document.children.borrow();
+    let html = dom_children
+        .iter()
+        .filter(|n| n.is_element())
+        .next()
+        .unwrap();
+    println!("{:?}", html.data);
+    let html_children = html.children.borrow();
+    //
+    let body = html_children
+        .iter()
+        .filter(|n| n.is_name(local_name!("body")))
+        .next()
+        .unwrap();
+    println!("{:?}", body.data);
+    // flat map
+    let all = html
+        .iter_self_and_descendant()
+        .filter(|n| n.is_name(local_name!("dl")))
+        .filter(|n| n.has_id());
+    for entry in all {
+        let id = entry.id().unwrap();
+        println!("id={}", id);
     }
 }
 
