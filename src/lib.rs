d03a837c3ad2fa60122271ab9dbb4f55c4a0c947
ekicyou <dot.station@gmail.com>
2020-11-17 16:36:12 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 mod api;
 mod enc;
+pub mod entry;
 mod error;
 mod gstr;
 mod parsers;
 mod windows;
-
-pub mod entry;
 
 /// prelude
 mod prelude {
     pub use crate::api::ShioriAPI;
     pub use crate::enc::Encoder;
     pub use crate::enc::Encoding;
+    pub use crate::entry;
     pub use crate::error::{ApiError, ApiResult};
     pub use crate::gstr::{GCowStr, GPath, GStr};
     pub use crate::parsers::req::ShioriRequest;
