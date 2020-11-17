b5f5610fc86a9147b3a514bcea57ca645ab9167a
ekicyou <dot.station@gmail.com>
2020-11-17 16:25:08 +0900

    シンプルにする

--- src/entry.rs
+++ src/entry.rs
@@ -38,79 +38,77 @@ const DLL_THREAD_ATTACH: DWORD = 2;
 #[allow(non_snake_case)]
 const DLL_THREAD_DETACH: DWORD = 3;
 
-impl<TAPI: ShioriAPI> ShioriEntry<TAPI> {
-    /// dllmain entry.
-    /// Save hinst only.
-    #[allow(dead_code)]
-    pub fn dllmain(hinst: usize, ul_reason_for_call: DWORD, _lp_reserved: LPVOID) -> bool {
-        match ul_reason_for_call {
-            DLL_PROCESS_ATTACH => {
-                set_hinst(hinst);
-                true
-            }
-            //DLL_PROCESS_DETACH => unload(),
-            _ => true,
+/// dllmain entry.
+/// Save hinst only.
+#[allow(dead_code)]
+pub fn dllmain(hinst: usize, ul_reason_for_call: DWORD, _lp_reserved: LPVOID) -> bool {
+    match ul_reason_for_call {
+        DLL_PROCESS_ATTACH => {
+            set_hinst(hinst);
+            true
         }
+        //DLL_PROCESS_DETACH => unload(),
+        _ => true,
     }
+}
 
-    /// load entry.
-    /// 1. Create shiori3 event stream.
-    /// 2. Send Load Event.
-    #[allow(dead_code)]
-    pub fn load(&mut self, hdir: HGLOBAL, len: usize) -> bool {
-        match self.load_impl(hdir, len) {
-            Err(e) => {
-                error!("{:?}", e);
-                false
-            }
-            _ => true,
+/// load entry.
+/// 1. Create shiori3 event stream.
+/// 2. Send Load Event.
+#[allow(dead_code)]
+pub fn load<T: ShioriAPI>(api: &Mutex<T>, hdir: HGLOBAL, len: usize) -> bool {
+    match impl_load(api, hdir, len) {
+        Err(e) => {
+            error!("{:?}", e);
+            false
         }
+        _ => true,
     }
-    fn load_impl(&mut self, hdir: HGLOBAL, len: usize) -> ApiResult<()> {
-        let mut lock = self.0.lock()?;
-        let load_dir = gstr::capture_path(hdir, len);
-        (*lock).api.load(hinst(), load_dir)
-    }
+}
+fn impl_load<T: ShioriAPI>(api: &Mutex<T>, hdir: HGLOBAL, len: usize) -> ApiResult<()> {
+    let mut lock = api.lock()?;
+    let load_dir = gstr::capture_path(hdir, len);
+    (*lock).load(hinst(), load_dir)
+}
 
-    /// unload entry.
-    /// 1. Send Unload Event.
-    /// 2. Drop shiori3 event stream.
-    #[allow(dead_code)]
-    pub fn unload(&mut self) -> bool {
-        match self.unload_impl() {
-            Err(e) => {
-                error!("{:?}", e);
-                false
-            }
-            _ => true,
+/// unload entry.
+/// 1. Send Unload Event.
+/// 2. Drop shiori3 event stream.
+#[allow(dead_code)]
+pub fn unload<T: ShioriAPI>(api: &Mutex<T>) -> bool {
+    match unload_impl(api) {
+        Err(e) => {
+            error!("{:?}", e);
+            false
         }
+        _ => true,
     }
-    fn unload_impl(&mut self) -> ApiResult<()> {
-        let mut lock = self.0.lock()?;
-        (*lock).api.unload()
-    }
+}
+fn unload_impl<T: ShioriAPI>(api: &Mutex<T>) -> ApiResult<()> {
+    let mut lock = api.lock()?;
+    (*lock).unload()
+}
 
-    /// request entry.
-    /// 1. Send Request Event.
-    /// 2. Wait event.result.
-    #[allow(dead_code)]
-    pub fn request(&mut self, hreq: HGLOBAL, len: &mut usize) -> HGLOBAL {
-        let req = gstr::capture_str(hreq, *len);
-        match self.request_impl(req) {
-            Err(e) => {
-                error!("{:?}", e);
-                *len = 0;
-                ptr::null_mut()
-            }
-            Ok(res) => {
-                let res = gstr::clone_from_str_nofree(&*res);
-                *len = res.len();
-                res.handle()
-            }
+/// request entry.
+/// 1. Send Request Event.
+/// 2. Wait event.result.
+#[allow(dead_code)]
+pub fn request<T: ShioriAPI>(api: &Mutex<T>, hreq: HGLOBAL, len: &mut usize) -> HGLOBAL {
+    let req = gstr::capture_str(hreq, *len);
+    match request_impl(api, req) {
+        Err(e) => {
+            error!("{:?}", e);
+            *len = 0;
+            ptr::null_mut()
+        }
+        Ok(res) => {
+            let res = gstr::clone_from_str_nofree(&*res);
+            *len = res.len();
+            res.handle()
         }
     }
-    fn request_impl(&mut self, req: GCowStr) -> ApiResult<String> {
-        let mut lock = self.0.lock()?;
-        (*lock).api.request(req)
-    }
+}
+fn request_impl<T: ShioriAPI>(api: &Mutex<T>, req: GCowStr) -> ApiResult<String> {
+    let mut lock = api.lock()?;
+    (*lock).request(req)
 }
